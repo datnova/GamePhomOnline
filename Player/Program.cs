16ab048150184eb7802fbf03e93824b815964345
datnova <dat6295th55@gmail.com>
2022-04-29 09:01:26 +0700

    Revert "test push"
    This reverts commit 5dca9b0dadaa2a2c41a18c26f6df61f7f323256f.

--- Player/Program.cs
+++ Player/Program.cs
@@ -17,7 +17,6 @@ namespace Player
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new PlayerForm());
-
         }
     }
 }
